a2af69f48bb92af420be49248cacb32914b09918
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-03-01 00:46:34 +0530

    chore: add typescript support

---
+++ www/src/pages/index.tsx
@@ -0,0 +1,8 @@
+const Welcome = () => {
+  return (
+    <div>
+      <div className="text-gray-500">Welcome to SMV. Test page to verify setup works</div>
+    </div>
+  );
+};
+export default Welcome;
