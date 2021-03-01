d2144a29fd8162e2b5fca64b8909a4862a28d5f8
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-03-01 23:34:52 +0530

    mock: create function to mock countries to visit on landing page

--- mock-api/routes/landing.js
+++ mock-api/routes/landing.js
@@ -3,7 +3,25 @@ async function landingRouter(app) {
     method: 'GET',
     url: '/countriestovisit',
     handler: (request, reply) => {
-      reply.send('Test');
+      reply.send(
+        [
+          {
+            "name": "Portugal",
+            "imageUrl": "local-img-url-here",
+            "processingTime": "5-6 working days"
+          },
+          {
+            "name": "Russia",
+            "imageUrl": "local-img-url-here",
+            "processingTime": "7-8 working days"
+          },
+          {
+            "name": "Australia",
+            "imageUrl": "local-img-url-here",
+            "processingTime": "7-8 working days"
+          },
+        ]
+      );
     }
   });
 };
