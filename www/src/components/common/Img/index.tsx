b003c62ab664202d82af6f19cd03bdd5dd6ee8bc
Girish <girishsawant999.gs@gmail.com>
2021-03-02 17:51:23 +0530

    feat: Phone number input screen login - signup

--- www/src/components/common/Img/index.tsx
+++ www/src/components/common/Img/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Image from 'next/image';
 
-index.propTypes = {};
+Img.propTypes = {};
 
-function index({ src, alt, className, style }) {
-  return <img src={src} alt={alt} className={className} style={style} />;
+function Img({ src, alt, className, style, width, height }) {
+  return <Image src={src} alt={alt} width={width} height={height} className={className} style={style} />;
 }
 
-export default index;
+export default Img;
