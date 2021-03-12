84d98dc174d8ac50051edeafab5f7281e9fb1324
Girish <girishsawant999.gs@gmail.com>
2021-03-12 19:33:58 +0530

    fix: Remove undefined type from button props

--- www/src/components/common/Button/index.tsx
+++ www/src/components/common/Button/index.tsx
@@ -3,12 +3,17 @@ import styles from './commonButton.module.css';
 
 type IButtonProps = {
   onClick: () => void;
-  type: 'button' | 'submit' | 'reset' | undefined;
+  type: 'button' | 'submit' | 'reset';
   className?: string;
   children: React.ReactChild | React.ReactChildren;
 };
 
-function Button({ onClick, className = '', children, type }: IButtonProps) {
+function Button({
+  onClick,
+  className = '',
+  children,
+  type = 'button'
+}: IButtonProps) {
   return (
     <button
       className={className.concat(' ').concat(styles.button)}
