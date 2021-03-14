570c2da1b7afd3cdf34435d1e659a3d7ebaa88ed
sandhu.rahul25 <sandhu.rahul25@gmail.com>
2021-03-14 13:01:27 +0530

    mock api update

--- api-mocks/countries/index.js
+++ api-mocks/countries/index.js
@@ -1,23 +1,28 @@
-const router = require("express").Router()
+const router = require('express').Router();
 
-router.get("/list", (req, res) => {
-    const response = {"success": true,
-    "data": [
-        {
-            "currency": null,
-            "currency_symbol": null,
-            "image_url": null,
-            "tags": [],
-            "is_deleted": false,
-            "updated_at": null,
-            "processing_time_in_days": 10,
-            "_id": "6040811ac49b47472cbdaa24",
-            "name": "India",
-            "symbol": "IND",
-            "created_at": "2021-03-04T06:41:30.726Z"
-        }
-    ]}
-    res.send(response)
-})
+router.get('/list', (req, res) => {
+  const response = {
+    success: true,
+    data: [
+      {
+        mainObj: {
+          subobj: { key: 'value' }
+        },
+        currency: null,
+        currency_symbol: null,
+        image_url: null,
+        tags: [],
+        is_deleted: false,
+        updated_at: null,
+        processing_time_in_days: 10,
+        _id: '6040811ac49b47472cbdaa24',
+        name: 'India',
+        symbol: 'IND',
+        created_at: '2021-03-04T06:41:30.726Z'
+      }
+    ]
+  };
+  res.send(response);
+});
 
 module.exports = router;
