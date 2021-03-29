167997db1124fc3f177797a0fe8bdc5f4766f7f7
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-03-29 21:07:25 +0530

    feat: modify the icon component to serve different icons based on breakpoints

--- www/src/components/common/Icon/index.tsx
+++ www/src/components/common/Icon/index.tsx
@@ -5,11 +5,24 @@ type IIconProps = {
 	src: string;
 	alt: string;
 	className?: string;
+	srcSet?: string;
 };
 
 const baseLocation = '/assets/icons/';
-function Icon({ src, alt, className }: IIconProps) {
-	return <img src={baseLocation + src} alt={alt} className={className} />;
+function Icon({ src, alt, className, srcSet }: IIconProps) {
+	return (
+		<img
+			src={baseLocation + src}
+			srcSet={srcSet
+				?.split(',')
+				.map((src) => {
+					return baseLocation + src.trim();
+				})
+				.join(',')}
+			alt={alt}
+			className={className + 'w-full h-full'}
+		/>
+	);
 }
 
 export default Icon;
